c694ed072e45381e9843f48ed8d3c222a7d20656
Joseph Daaboul <joseph.jd.daaboul@gmail.com>
2016-10-21 15:10:19 -0400

    #21 vendor repository interface and implementation
    throw exception until DB is set up

---
+++ Glimpse.Core/Contracts/Repository/IVendorRepository .cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Glimpse.Core.Model;
+
+namespace Glimpse.Core.Contracts.Repository
+{
+    public interface IVendorRepository
+    {
+        Task<User> SearchUser(string userName);
+
+        Task<User> Login(string userName, string password);
+
+        Task SignUp(string userName, string password, string email, string company);
+    }
+}
