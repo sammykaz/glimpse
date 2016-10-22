8d2123a2ca873406c6c9726c48c186d4ac74c194
Sam Kazerouni <samkazerouni@gmail.com>
2016-10-22 18:40:02 -0400

    Added Configuration files for database

---
+++ Glimpse.Core/Model/TempVendor.cs
@@ -0,0 +1,25 @@
+﻿using SQLite.Net.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Glimpse.Core.Model
+{
+    class TempVendor
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string password { get; set; }
+        public string email { get; set; }
+        public string company { get; set; }
+        public int companyId { get; set; }
+        public string personalPhoneNumber { get; set; }
+        public string businessPhoneNumber { get; set; }
+
+    }
+}
