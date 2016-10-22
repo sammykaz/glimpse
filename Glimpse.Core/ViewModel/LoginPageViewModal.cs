1a8cea8ea508c0505d832b92bad0a85d61ef4aef
Omer <bosniak.bos@gmail.com>
2016-10-22 19:39:51 -0400

    #30 Created the Fragment, Viewmodal and drawable for the login page. (The page is still not the first page that opens on app start up)

---
+++ Glimpse.Core/ViewModel/LoginPageViewModal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MvvmCross.Plugins.Messenger;
+
+namespace Glimpse.Core.ViewModel
+{
+    public class LoginPageViewModal : BaseViewModel
+    {
+        public LoginPageViewModal(IMvxMessenger messenger) : base(messenger)
+        {
+
+        }
+
+        public void ShowLoginPage()
+        {
+            ShowViewModel<LoginPageViewModal>();
+        }
+
+    }
+}
