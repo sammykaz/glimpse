9ab7fef18b08255df76f94006fd611380f85e34c
Joseph Daaboul <joseph.jd.daaboul@gmail.com>
2016-11-03 23:20:33 -0400

    renaming from mytrains to glimpse @sammykaz

--- Glimpse.Core/ViewModel/LoginPageViewModel.cs
+++ Glimpse.Core/ViewModel/LoginPageViewModel.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MvvmCross.Plugins.Messenger;
+﻿using MvvmCross.Plugins.Messenger;
 using MvvmCross.Core.ViewModels;
-using MyTrains.Core.ViewModel;
 
 namespace Glimpse.Core.ViewModel
 {
