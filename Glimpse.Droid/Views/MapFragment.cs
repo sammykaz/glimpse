6f583e48bab66bae59948c550e48d048d5c84c88
RiceAndBeanz <eleonrodas@gmail.com>
2016-11-04 19:25:09 -0400

    #39  Map now loads inside the Mapfragment as a map view.
    @asmalaaribi

--- Glimpse.Droid/Views/MapFragment.cs
+++ Glimpse.Droid/Views/MapFragment.cs
@@ -2,68 +2,106 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using MvvmCross.Droid.Shared.Attributes;
-using MvvmCross.Binding.Droid.BindingContext;
 using MvvmCross.Droid.Support.V7.Fragging.Fragments;
 using Glimpse.Droid.Extensions;
 using Glimpse.Droid.Activities;
 using MyTrains.Core.ViewModel;
 using Glimpse.Droid;
-using Android.OS;
-using Android.App;
 using Android.Gms.Maps;
-using System;
 using Android.Gms.Maps.Model;
-using MvvmCross.Droid.Support.V7.Fragging;
-using MvvmCross.Binding.BindingContext;
-using Glimpse.Droid.Helpers;
 
 namespace MyTrains.Droid.Views
 
 {
-    [Activity(Label = "View for MapViewModel")]
-    public class MapView : MvxFragmentActivity
+    [MvxFragment(typeof(Glimpse.Core.ViewModel.MainViewModel), Resource.Id.content_frame, true)]
+    [Register("mytrains.droid.views.MapFragment")]
+    public class MapFragment : MvxFragment<MapViewModel>
     {
-       
+        private MapView _mapView;
+        private GoogleMap _map;
         private Marker _store;
 
 
-        protected override void OnCreate(Bundle bundle)
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
-            base.OnCreate(bundle);
-            SetContentView(Resource.Layout.MapView);
+            var view = inflater.Inflate(Resource.Layout.MapView, null);
+            _mapView = view.FindViewById<MapView>(Resource.Id.map);
+            _mapView.OnCreate(savedInstanceState);
+            return view ;
+        }
 
+        public override void OnActivityCreated(Bundle p0)
+        {
+            base.OnActivityCreated(p0);
+            (this.Activity as MainActivity).SetCustomTitle("MapView");
+            MapsInitializer.Initialize(Activity);
             var viewModel = (MapViewModel)ViewModel;
+        }
 
-            var mapFragment = (SupportMapFragment)SupportFragmentManager.FindFragmentById(Resource.Id.map);
+        public override void OnStart()
+        {
+            base.OnStart();
+            InitializeMapAndHandlers();
+        }
 
-            var options = new MarkerOptions();
-            options.SetPosition(new LatLng(viewModel.Store.Location.Lat, viewModel.Store.Location.Lng));
-            options.SetTitle("Store");
-            _store = mapFragment.Map.AddMarker(options);
+        private void InitializeMapAndHandlers()
+        {
+            SetUpMapIfNeeded();
 
-            LatLng location = new LatLng(45.5017, -73.5673);
-            CameraPosition.Builder builder = CameraPosition.InvokeBuilder();
-            builder.Target(location);
-            builder.Zoom(viewModel.DefaulZoom);
-            builder.Bearing(viewModel.DefaultBearing);
-            builder.Tilt(viewModel.DefaultTilt);
-            CameraPosition cameraPosition = builder.Build();
-            CameraUpdate cameraUpdate = CameraUpdateFactory.NewCameraPosition(cameraPosition);
-
-           
-            GoogleMap map = mapFragment.Map;
-            if (map != null)
+            if (_map != null)
             {
-                map.MoveCamera(cameraUpdate);
+                var viewModel = (MapViewModel)ViewModel;
+
+                var options = new MarkerOptions();
+                options.SetPosition(new LatLng(viewModel.Store.Location.Lat, viewModel.Store.Location.Lng));
+                options.SetTitle("Store");
+                _store = _map.AddMarker(options);
+
+                LatLng location = new LatLng(45.5017, -73.5673);
+                CameraPosition.Builder builder = CameraPosition.InvokeBuilder();
+                builder.Target(location);
+                builder.Zoom(viewModel.DefaulZoom);
+                builder.Bearing(viewModel.DefaultBearing);
+                builder.Tilt(viewModel.DefaultTilt);
+                CameraPosition cameraPosition = builder.Build();
+                CameraUpdate cameraUpdate = CameraUpdateFactory.NewCameraPosition(cameraPosition);
+                _map.MoveCamera(cameraUpdate);
             }
+        }
 
-            var set = this.CreateBindingSet<MapView, MapViewModel>();
-            set.Bind(_store)
-               .For(m => m.Position)
-               .To(vm => vm.Store.Location)
-               .WithConversion(new LatLngValueConverter(), null);
-            set.Apply();
 
+        public override void OnDestroyView()
+        {
+            base.OnDestroyView();
+            _mapView.OnDestroy();
+        }
+
+        public override void OnResume()
+        {
+            base.OnResume();
+            SetUpMapIfNeeded();
+            _mapView.OnResume();
+        }
+
+        public override void OnPause()
+        {
+            base.OnPause();
+            _mapView.OnPause();
+        }
+
+        public override void OnLowMemory()
+        {
+            base.OnLowMemory();
+            _mapView.OnLowMemory();
+        }
+
+        private void SetUpMapIfNeeded()
+        {
+            if (null == _map)
+            {
+                _map = View.FindViewById<MapView>(Resource.Id.map).Map;
+            }
         }
     }
+
 }
