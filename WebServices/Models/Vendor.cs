2e7b3cfac900f9806723efc67812713444a9288c
Sam Kazerouni <samkazerouni@gmail.com>
2016-11-04 20:39:42 -0400

    Added Vendor Model in WebServices

---
+++ WebServices/Models/Vendor.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Glimpse.Core.Model
+{
+    public class Vendor
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string Company { get; set; }
+    }
+}
