54a75484c5e847cb8627317bee02029b88a2682a
Sam Kazerouni <samkazerouni@gmail.com>
2016-11-04 20:40:51 -0400

    #21 Removed unused imports

--- Glimpse.Core/Model/Vendor.cs
+++ Glimpse.Core/Model/Vendor.cs
@@ -1,25 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Glimpse.Core.Model
 {
-    public class Vendor : User
+    public class Vendor
     {
-        private string _company;
+        public string FirstName { get; set; }
 
-        public Vendor(string firstName, string email, string password,  string company) //: base(firstName, email, password)
-        {
-            _company = company;
-        }                
+        public string Email { get; set; }
 
-        public string Company
-        {
-            get { return _company; }
-            set { _company = value; }
-        }       
+        public string Password { get; set; }
 
+        public string Company { get; set; }
     }
 }
