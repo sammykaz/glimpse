db2f75d089e201e882ce9520b32d405aa54287e2
Joseph Daaboul <joseph.jd.daaboul@gmail.com>
2016-11-08 20:59:18 -0500

    removed unneeded services

--- Glimpse.Core/Contracts/Services/IDialogService.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace Glimpse.Core.Contracts.Services
-{
-    public interface IDialogService
-    {
-        Task ShowAlertAsync(string message, string title, string buttonText);
-    }
-}
