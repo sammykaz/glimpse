336014c77691a59b5348194c3ab8076a93276174
Joseph Daaboul <joseph.jd.daaboul@gmail.com>
2016-11-11 19:25:53 -0500

    initialize async

--- Glimpse.Core/ViewModel/MapViewModel.cs
+++ Glimpse.Core/ViewModel/MapViewModel.cs
@@ -6,6 +6,9 @@ using System.Collections.ObjectModel;
 using Glimpse.Core.Contracts.Services;
 using System.Threading.Tasks;
 using Glimpse.Core.Extensions;
+using Plugin.Geolocator;
+using Plugin.Geolocator.Abstractions;
+using MvvmCross.Platform.WeakSubscription;
 
 namespace Glimpse.Core.ViewModel
 {
@@ -18,14 +21,58 @@ namespace Glimpse.Core.ViewModel
         private Store _store;
         private ObservableCollection<Store> _stores;
         private IStoreDataService _storeDataService;
+        private IGeolocator locator;
+        //locator.DesiredAccuracy = 50;
+
 
 
         public MapViewModel(IMvxMessenger messenger,  IStoreDataService storeDataService) : base(messenger)
 
         {
             _storeDataService = storeDataService;
-            LoadStores();
+           /* locator = CrossGeolocator.Current;
+            locator.DesiredAccuracy = 50;
+
+            locator.PositionChanged += (sender, e) => {
+                var position = e.Position;
+
+                var location = new Location()
+                {
+                    Lat = position.Latitude,
+                    Lng = position.Longitude
+                };
+
+                Store.Location = location;
+            }; */
+
         }
+
+        public override async void Start()
+        {
+            base.Start();
+            await ReloadDataAsync();
+        }
+
+        protected override async Task InitializeAsync()
+        {
+            _stores = (await _storeDataService.GetAllStores()).ToObservableCollection();
+            Store = Stores[0];
+
+            /*var position = await locator.GetPositionAsync(timeoutMilliseconds: 10000);
+
+            new Store()
+            {
+                Name = "Store",
+                Location = new Location()
+                {
+                    Lat = position.Latitude,
+                    Lng = position.Longitude
+                }
+            }; */
+        }
+
+
+
 
 
         public int DefaulZoom
@@ -71,23 +118,23 @@ namespace Glimpse.Core.ViewModel
         internal async Task LoadStores()
 
         {
-            _stores = (await _storeDataService.GetAllStores()).ToObservableCollection();
-            Store = Stores[0];
+            //_stores = (await _storeDataService.GetAllStores()).ToObservableCollection();
+            //Store = Stores[0];
+
+            var position = await locator.GetPositionAsync(timeoutMilliseconds: 10000);
+
+            new Store()
+            {
+                Name = "Store",
+                Location = new Location()
+                {
+                    Lat = position.Latitude,
+                    Lng = position.Longitude
+                }
+            };
         }
 
-    /*    protected override async Task InitializeAsync()
-        {
-            await base.InitializeAsync();
-
-           
-                await LoadStores();
-
-                Store = Stores[0];
-
-
-
-           
-        }*/
+        
 
         public MvxCommand MoveCommand
         {
@@ -108,6 +155,8 @@ namespace Glimpse.Core.ViewModel
                 });
             }
         }
+
+       // private MvxNamedNotifyPropertyChangedEventSubscription<> _selectedItemToken;
 
 
     }
