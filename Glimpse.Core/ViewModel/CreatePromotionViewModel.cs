b460b35ad9823f2fccb64ed07ac9950fc1f0adf7
Omer <bosniak.bos@gmail.com>
2016-11-13 15:46:52 -0500

    #61 Linked the Create promotion to the vendor profile

---
+++ Glimpse.Core/ViewModel/CreatePromotionViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MvvmCross.Plugins.Messenger;
+
+namespace Glimpse.Core.ViewModel
+{
+    public class CreatePromotionViewModel : BaseViewModel
+    {
+        public CreatePromotionViewModel(IMvxMessenger messenger) : base(messenger)
+        {
+
+        }
+
+        private string _promotionTitle;
+        public string PromotionTitle
+        {
+            get { return _promotionTitle; }
+            set
+            {
+                _promotionTitle = value;
+                RaisePropertyChanged(() => PromotionTitle);
+
+            }
+        }
+
+        private string _promotionDescription;
+        public string PromotionDescription
+        {
+            get { return _promotionDescription; }
+            set
+            {
+                _promotionDescription = value;
+                RaisePropertyChanged(() => PromotionDescription);
+
+            }
+        }
+
+    }
+}
