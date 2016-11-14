ea9f11bf5b6650a4a1e7014868a20dcf9798da70
Sam Kazerouni <samkazerouni@gmail.com>
2016-11-14 02:10:47 -0500

    Added new Models to WebServices

---
+++ WebServices/Models/Telephone.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Glimpse.Core.Model
+{
+    public class Telephone
+    {
+        public string PersonalPhoneNumber { get; set; }
+        public string BusinessPhoneNumber { get; set; }
+    }
+}
