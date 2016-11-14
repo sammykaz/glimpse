15d27130525aba2bfe317387ed954b67b19c93a5
Sam Kazerouni <samkazerouni@gmail.com>
2016-11-14 02:03:15 -0500

    #62 LoginViewModel contains sign in and sign up logic

---
+++ Glimpse.Core/ViewModel/LoginViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using MvvmCross.Core.ViewModels;
+using MvvmCross.Plugins.Messenger;
+using Glimpse.Core.Model.App;
+using Glimpse.Core.Utility;
+
+namespace Glimpse.Core.ViewModel
+{
+    public class LoginViewModel: BaseViewModel
+    {
+        public LoginViewModel(IMvxMessenger messenger) : base(messenger)
+        {
+           
+        }
+        public MvxCommand ShowVendorSignUp
+        {
+            get { return ShowCommand<VendorSignUpViewModel>(); }
+        }
+
+        public MvxCommand ShowMapView
+        {
+            get { return ShowCommand<MapViewModel>(); }
+        }
+
+        public MvxCommand ShowUserSignUp
+        {
+            get { return ShowCommand<UserSignUpViewModel>(); }
+        }
+
+        public MvxCommand ShowSignIn
+        {
+            get { return ShowCommand<SignInViewModel>(); }
+        }
+
+
+        private MvxCommand ShowCommand<TViewModel>()
+            where TViewModel : IMvxViewModel
+        {
+            return new MvxCommand(() => ShowViewModel<TViewModel>());
+        }
+
+    }
+}
