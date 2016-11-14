d41c270cd4a5ca36cf19e38cc62a4af0b3517896
Sam Kazerouni <samkazerouni@gmail.com>
2016-11-14 03:38:15 -0500

    Added new Models

--- Glimpse.Core/Model/Vendor.cs
+++ Glimpse.Core/Model/Vendor.cs
@@ -1,16 +1,23 @@
-﻿
-namespace Glimpse.Core.Model
+﻿namespace Glimpse.Core.Model
 {
     public class Vendor
     {
         public string FirstName { get; set; }
 
+        public string LastName { get; set; }
+
+        public string UserName { get; set; }
+
         public string Email { get; set; }
 
         public string Password { get; set; }
 
-        public string Company { get; set; }
+        public string CompanyName { get; set; }
 
         public string Salt { get; set; }
+
+        public Address Address { get; set; }
+
+        public Telephone Telephone { get; set; }
     }
 }
