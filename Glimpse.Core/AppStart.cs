d388a897218e0dedb67a043ac42f9be4d0daa673
Sam Kazerouni <samkazerouni@gmail.com>
2016-11-14 01:55:50 -0500

    #62 Added temporary logic for login feature

--- Glimpse.Core/AppStart.cs
+++ Glimpse.Core/AppStart.cs
@@ -1,15 +1,49 @@
 ﻿using MvvmCross.Core.ViewModels;
 using MvvmCross.Platform;
 using Glimpse.Core.Contracts.Services;
+using Glimpse.Core.Services.Data;
 using Glimpse.Core.ViewModel;
+
 
 namespace Glimpse.Core
 {
     public class AppStart: MvxNavigatingObject, IMvxAppStart
     {
+       public static bool IsUserLoggedIn { get; set; }
+
+        
         public void Start(object hint = null)
         {
-            ShowViewModel<MainViewModel>();
+            IsUserLoggedIn = false;
+            if (!IsUserLoggedIn)
+            {
+                ShowViewModel<LoginMainViewModel>();
+            }
+            else
+            {
+                ShowViewModel<MainViewModel>();
+            }
+            
+
+            //var userService = Mvx.Resolve<UserDataService>();
+            // var vendorService = Mvx.Resolve<VendorDataService>();
+
+            // var activeUser = userService.GetActiveUser();
+            // var activeVendor = vendorService.GetActiveVendor();
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+            
         }
     }
 }
