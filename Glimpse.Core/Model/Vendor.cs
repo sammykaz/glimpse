d06ac3e9ebb53713b1b8510674c770a80def068a
Sam Kazerouni <samkazerouni@gmail.com>
2016-11-16 15:29:22 -0500

    #16 Added new Vendor and User Models @PhillipsJP

--- Glimpse.Core/Model/Vendor.cs
+++ Glimpse.Core/Model/Vendor.cs
@@ -1,16 +1,42 @@
 ﻿
+using System.Collections.Generic;
+
 namespace Glimpse.Core.Model
 {
     public class Vendor
     {
         public string FirstName { get; set; }
 
+        public string LastName { get; set; }
+
+        public string UserName { get; set; }
+
         public string Email { get; set; }
 
         public string Password { get; set; }
 
-        public string Company { get; set; }
+        public string CompanyName { get; set; }
 
         public string Salt { get; set; }
+
+        public Address Address { get; set; }
+
+        public Telephone Telephone { get; set; }
+
+
+        public List<Promotion> Promotions { get; set; }
+
+
+        private Location _location;
+
+        public Location Location
+        {
+            get { return _location; }
+            set
+            {
+                Location LatLngAddress;
+                _location = Utility.Geocoding.Geocode(Address, out LatLngAddress);
+            }
+        }
     }
 }
