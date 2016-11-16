1665887edb20c0aca2b52b2ecdb0184a439f49f4
Sam Kazerouni <samkazerouni@gmail.com>
2016-11-16 15:28:53 -0500

    #16 Added Geocoding class @PhillipsJP

---
+++ Glimpse.Core/Utility/Geocoding.cs
@@ -0,0 +1,40 @@
+﻿using Glimpse.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Glimpse.Core.Utility
+{
+    public class Geocoding
+    {
+        public static Location Geocode(string addressToConvert, out Location location)
+        {
+
+            //TODO Complete geocoding to enable for Country, Province, City, Address..
+
+            var address = addressToConvert;
+            var requestUri = string.Format("http://maps.googleapis.com/maps/api/geocode/xml?address={0}&sensor=false", Uri.EscapeDataString(address));
+            
+            var request = WebRequest.Create(requestUri);
+            var response = request.GetResponseAsync();
+
+
+            var xdoc = XDocument.Load(response.Result.GetResponseStream());
+
+            var result = xdoc.Element("GeocodeResponse").Element("result");
+            var locationElement = result.Element("geometry").Element("location");
+            var lat = locationElement.Element("lat");
+            var lng = locationElement.Element("lng");
+
+            double latitude = Double.Parse(lat.Value);
+            double longitude = Double.Parse(lng.Value);
+
+            location = new Location(latitude,longitude);
+            return location;
+        } 
+    }
+}
