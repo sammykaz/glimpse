112bd64832d9a74a82c5b9bd7c8475fd1f47aadf
Sam Kazerouni <samkazerouni@gmail.com>
2016-11-16 15:29:48 -0500

    #16 Added Promotion model @PhillipsJP

---
+++ Glimpse.Core/Model/Promotion.cs
@@ -0,0 +1,67 @@
+﻿
+using MvvmCross.Core.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Glimpse.Core.Model
+{
+    public class Promotion
+    {
+        public string _title;
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+
+        public string _description;
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+
+        private List<Category> _categories;
+        public List<Category> Categories
+        {
+            get { return _categories; }
+            set
+            {
+                if (!Category.IsDefined(typeof(Category), value))
+                {
+                    _categories = null;
+                }
+
+                _categories = value;
+            }
+        }
+
+        //These dates will be extracted from a calendar UI in the future.
+        public string PromotionStartDate { get; set; }
+
+        public string PromotionEndDate { get; set; }
+
+        public bool PromotionActive;
+
+
+        //Add images here
+
+        //Any promotion logic below
+
+    }
+
+    //Category Types for Promotions
+    public enum Category
+    {
+        Footwear,
+        Electronics,
+        Jewellery,
+        Restaurants,
+        Services,
+        Apparel
+    }
+
+}
