b3d90ab94dfc9d5125e4d782496068c78b3e6f6c
Sam Kazerouni <samkazerouni@gmail.com>
2016-11-19 02:14:24 -0500

    #62 Added Logout to navigation bar

--- Glimpse.Core/ViewModel/MenuViewModel.cs
+++ Glimpse.Core/ViewModel/MenuViewModel.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using Glimpse.Core.Contracts.Services;
 using MvvmCross.Core.ViewModels;
 using MvvmCross.Plugins.Messenger;
 using Glimpse.Core.Model.App;
+using Glimpse.Core.Services.General;
 using Glimpse.Core.Utility;
+using MvvmCross.Platform;
 
 namespace Glimpse.Core.ViewModel
 {
@@ -14,14 +17,28 @@ namespace Glimpse.Core.ViewModel
 
         public event EventHandler CloseMenu;
 
-        public MenuViewModel(IMvxMessenger messenger) : base(messenger)
+        private ILoginDataService _loginDataService;
+
+        private string selectedMenuOption;
+
+
+
+        public MenuViewModel(IMvxMessenger messenger, ILoginDataService loginDataService) : base(messenger)
         {
             MenuItems = new ObservableCollection<MenuItem>();
             CreateMenuItems();
+            _loginDataService = loginDataService;
         }
 
         private void CreateMenuItems()
         {
+            MenuItems.Add(new MenuItem
+            {
+                Title = "Logout",
+                ViewModelType = typeof(LoginMainViewModel),
+                Option = MenuOption.Logout,
+                IsSelected = false
+            });
             MenuItems.Add(new MenuItem
             {
                 Title = "Buyer Profile Page",
@@ -35,13 +52,29 @@ namespace Glimpse.Core.ViewModel
                 ViewModelType = typeof(VendorProfilePageViewModel),
                 Option = MenuOption.VendorProfile,
                 IsSelected = false
-            });          
+            });
         }
 
         private void OnMenuEntrySelect(MenuItem item)
         {
-            ShowViewModel(item.ViewModelType);
+            if (item.Option == MenuOption.Logout)
+            {
+                _loginDataService.ClearCredentials();
+                _loginDataService.ClearLoginState();
+            }
+                ShowViewModel(item.ViewModelType);
+            
             RaiseCloseMenu();
+        }
+
+        public void SetSelectedMenuOption(string menuOption)
+        {
+            this.selectedMenuOption = menuOption;
+        }
+
+        public string GetSelectedMenuOption()
+        {
+            return this.selectedMenuOption;
         }
 
         private void RaiseCloseMenu()
