052140a864feac74ded640678730b009fd02378f
Sam Kazerouni <samkazerouni@gmail.com>
2016-11-19 02:15:24 -0500

    #62 Added LoginDataService for login logic

---
+++ Glimpse.Core/Contracts/Services/ILoginDataService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Glimpse.Core.Model;
+
+namespace Glimpse.Core.Contracts.Services
+{
+    public interface ILoginDataService
+    {
+        bool AuthenticateVendor(Vendor vendor, string username, string password);
+        bool AuthenticateUser(User user, string username, string password);
+        bool AuthenticateUserLogin();
+        void ClearCredentials();
+        void ClearLoginState();
+    }
+}
