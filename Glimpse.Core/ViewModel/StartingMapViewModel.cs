459c6f2b1c2b7771ed745fc72ca0c5fd98ea7806
Sam Kazerouni <samkazerouni@gmail.com>
2016-11-19 02:50:40 -0500

    #62 Added StartingMapViewModel

---
+++ Glimpse.Core/ViewModel/StartingMapViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using MvvmCross.Core.ViewModels;
+using MvvmCross.Platform;
+using Glimpse.Core.Contracts.ViewModel;
+using Glimpse.Core.ViewModel;
+
+
+namespace Glimpse.Core.ViewModel
+{
+    public class StartingMapViewModel : MvxViewModel, IMainViewModel
+    {
+
+        public StartingMapViewModel()
+        {
+        }
+
+
+        public void ShowStartingMap()
+        {
+            ShowViewModel<StartingMapViewModel>();
+        }
+
+    }
+}
