24fe819a3ac44cdf8b1f14f07901424e2af2a301
Omer <bosniak.bos@gmail.com>
2016-11-25 18:15:54 -0500

    #61 Created the viewmodel, added the media plugin

---
+++ Glimpse.Core/ViewModel/CreatePromotionPart2ViewModel.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MvvmCross.Plugins.Messenger;
+using MvvmCross.Core.ViewModels;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+
+namespace Glimpse.Core.ViewModel
+{
+    public class CreatePromotionPart2ViewModel : BaseViewModel
+    {
+
+        public CreatePromotionPart2ViewModel(IMvxMessenger messenger) : base(messenger)
+        {
+        }
+
+
+        Dictionary<string, string> dataFromCreatePromotionPart1 = new Dictionary<string, string>();
+
+        protected override void InitFromBundle(IMvxBundle parameters)
+        {
+           // var mykey1value = parameters.Data["key1"];
+           
+            var myPara = parameters;
+            var service = myPara.Data["ServicesIsChecked"];
+            
+            foreach (string key in parameters.Data.Keys)
+            {
+                dataFromCreatePromotionPart1.Add(key, parameters.Data[key]);
+            }
+            // And so on
+            int i = 0;
+            base.InitFromBundle(parameters);
+        }
+
+      
+
+        private List<String> _lengthOfThePromotion = new List<String>()
+            {
+                "1","2","3","4","5","6","7"
+            };
+
+        public List<String> LengthOfThePromotion
+        {
+            get { return _lengthOfThePromotion; }
+            set { _lengthOfThePromotion = value; RaisePropertyChanged(() => LengthOfThePromotion); }
+        }
+
+        private String _selectedLenghtOfThePromotion;
+        public String SelectedLengthOfThePromotion
+        {
+            get { return _selectedLenghtOfThePromotion; }
+            set { _selectedLenghtOfThePromotion = value; RaisePropertyChanged(() => SelectedLengthOfThePromotion); }
+        }
+
+
+        private MediaFile _file;
+        public MediaFile File
+        {
+            get { return _file; }
+            set { _file = value; RaisePropertyChanged(() => File); }
+        }
+
+        private string _foo;
+        public string Foo
+        {
+            get { return _foo; }
+            set { _foo = value; RaisePropertyChanged(() => Foo); }
+        }
+      
+
+    public MvxCommand selectImg
+        {
+            get
+            {
+                return new MvxCommand(async () =>
+                {
+                    if (!CrossMedia.Current.IsPickPhotoSupported)
+                    {
+                        // DisplayAlert("Photos Not Supported", ":( Permission not granted to photos.", "OK");
+                        return;
+                    }
+                    File = await CrossMedia.Current.PickPhotoAsync();
+
+
+                    if (File == null)
+                        return;
+
+//                    image.Source = image.FromStream(() =>
+   //                 {
+   //                     var stream = file.GetStream();
+    //                    file.Dispose();
+    //                    return stream;
+     //               });
+
+                });
+
+            }
+        }
+
+
+    }
+}
---
+++ Glimpse.Core/ViewModel/CreatePromotionViewModel.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MvvmCross.Plugins.Messenger;
+using MvvmCross.Core.ViewModels;
+
+namespace Glimpse.Core.ViewModel
+{
+    public class CreatePromotionViewModel : BaseViewModel
+    {
+        private IMvxMessenger messenger;
+        public CreatePromotionViewModel(IMvxMessenger messenger) : base(messenger)
+        {
+
+        }
+
+        public MvxCommand ContinueCommand
+        {
+            get
+            {
+
+
+                return new MvxCommand(() =>
+                {
+                    var desc = new Dictionary<string, string> {
+                        {"PromotionTitle", PromotionTitle},{"PromotionDescription", PromotionDescription}, {"FootwearIsChecked", FootwearIsChecked.ToString()},
+                        {"ElectronicIsChecked", ElectronicIsChecked.ToString() },  {"JewlleryIsChecked", JewlleryIsChecked.ToString() }, {"RestaurantsIsChecked", RestaurantsIsChecked.ToString() },
+                        {"ServicesIsChecked", ServicesIsChecked.ToString() }, {"ApparelIsChecked", ApparelIsChecked.ToString()}
+                    };
+                                     
+                    ShowViewModel<CreatePromotionPart2ViewModel>(desc);
+                });
+                
+            }
+        }
+
+        private string _promotionTitle;
+        public string PromotionTitle
+        {
+            get { return _promotionTitle; }
+            set
+            {
+                _promotionTitle = value;
+                RaisePropertyChanged(() => PromotionTitle);
+            }
+        }
+        
+        private string _promotionDescription;
+        public string PromotionDescription
+        {
+            get { return _promotionDescription; }
+            set
+            {
+                _promotionDescription = value;
+                RaisePropertyChanged(() => PromotionDescription);
+
+            }
+        }
+
+        private bool _footwearIsChecked;
+        public bool FootwearIsChecked
+        {
+            get { return _footwearIsChecked; }
+            set
+            {
+                _footwearIsChecked = value;
+                RaisePropertyChanged(() => FootwearIsChecked);
+            }
+        }
+        private bool _electronicIsChecked;
+        public bool ElectronicIsChecked
+        {
+            get { return _electronicIsChecked; }
+            set
+            {
+                _electronicIsChecked = value;
+                RaisePropertyChanged(() => ElectronicIsChecked);
+            }
+        }
+        private bool _jewelleryIsChecked;
+        public bool JewlleryIsChecked
+        {
+            get { return _jewelleryIsChecked; }
+            set
+            {
+                _jewelleryIsChecked = value;
+                RaisePropertyChanged(() => JewlleryIsChecked);
+            }
+        }
+        private bool _restaurantsIsChecked;
+        public bool RestaurantsIsChecked
+        {
+            get { return _restaurantsIsChecked; }
+            set
+            {
+                _restaurantsIsChecked = value;
+                RaisePropertyChanged(() => RestaurantsIsChecked);
+            }
+        }
+        private bool _servicesIsChecked;
+        public bool ServicesIsChecked
+        {
+            get { return _servicesIsChecked; }
+            set
+            {
+                _servicesIsChecked = value;
+                RaisePropertyChanged(() => ServicesIsChecked);
+            }
+        }
+        private bool _apparelIsChecked;
+        public bool ApparelIsChecked
+        {
+            get { return _apparelIsChecked; }
+            set
+            {
+                _apparelIsChecked = value;
+                RaisePropertyChanged(() => ApparelIsChecked);
+            }
+        }
+
+
+    }
+}
