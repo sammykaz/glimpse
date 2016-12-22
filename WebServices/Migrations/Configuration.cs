e44dbd53411842c4f37e08a744170303df51245d
Joseph Daaboul <joseph.jd.daaboul@gmail.com>
2016-12-22 16:17:02 -0500

    migration stuff #103 for models

--- WebServices/Migrations/Configuration.cs
+++ WebServices/Migrations/Configuration.cs
@@ -1,6 +1,7 @@
 namespace WebServices.Migrations
 {
-    using Glimpse.Core.Model;
+    using Glimpse.Core.Services.General;
+    using Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -15,92 +16,47 @@ namespace WebServices.Migrations
 
         protected override void Seed(WebServices.Models.GlimpseDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
+           // Tuple<string, string> passwordTuple = Cryptography.EncryptAes("password");
+            //string password = passwordTuple.Item1.
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
 
             context.Users.AddOrUpdate(
-              x => x.UserId, new User()
-              {
-                  UserId = 1,
-                  FirstName = "sam",
-                  LastName = "kaz",
-                  Email = "sam@gmail.com",
-                  Password = "password",
-                  Salt = "salt",
-                  IsVendor = false
-              },
+             x => x.UserId, new User()
+             {
+                 UserId = 1,                
+                 Email = "sam@gmail.com",
+                 Password = "password",
+                 Salt = "salt"                 
+             },
+             new User()
+             {
+                 UserId = 2,               
+                 Email = "jo@gmail.com",
+                 Password = "password",
+                 Salt = "salt"
+             },
               new User()
               {
-                  UserId = 2,
-                  FirstName = "jo",
-                  LastName = "bool",
-                  Email = "jo@gmail.com",
+                  UserId = 3,                
+                  Email = "eric@gmail.com",
                   Password = "password",
-                  Salt = "salt",
-                  IsVendor = false
+                  Salt = "salt"
               },
                new User()
                {
-                   UserId = 3,
-                   FirstName = "eric",
-                   LastName = "leon",
-                   Email = "eric@gmail.com",
+                   UserId = 4 ,                 
+                   Email = "john@gmail.com",
                    Password = "password",
-                   Salt = "salt",
-                   IsVendor = false
+                   Salt = "salt"                  
                },
                 new User()
                 {
-                    UserId = 4,
-                    FirstName = "john",
-                    LastName = "doe",
+                    UserId = 5,                   
                     Email = "john@gmail.com",
                     Password = "password",
-                    Salt = "salt",
-                    IsVendor = false
-                },
-                 new User()
-                 {
-                     UserId = 5,
-                     FirstName = "john",
-                     LastName = "smith",
-                     Email = "john@gmail.com",
-                     Password = "password",
-                     Salt = "salt",
-                     IsVendor = false
-                 }
-              );
-
-
-            context.Vendors.AddOrUpdate(
-                 x => x.VendorId, new Vendor()
-                 {
-                     VendorId = 1,
-                     FirstName = "sam",
-                     LastName = "kaz",
-                     Email = "sam@gmail.com",
-                     Password = "password",
-                     CompanyName = "sam's Company",
-                     Salt = "salt",
-                     Address = new Address()
-                     {
-                         Country = "Canada",
-                         Province = "Quebec",
-                         City = "Montreal",
-                         PostalCode = "H4X1C2",
-                         Street = "7140",
-                         StreetNumber = "Sherbrooke O"
-                     },
-                     Telephone = new Telephone()
-                     {
-                         PersonalPhoneNumber = "51454363654",
-                         BusinessPhoneNumber = "51454363624"
-                     },
-                     Location = new Location(45.458151, -73.640116),
-                     IsVendor = false
-                 });
+                    Salt = "salt"                   
+                }
+             );
         }
     }
 }
