0223b5b623e2fec58f1ad61216c60d86281f92e8
asmalaaribi <asmalaaribi@gmail.com>
2016-12-27 10:37:05 -0500

    #87 small change

--- WebServices/src/controllers/mapController.js
+++ WebServices/src/controllers/mapController.js
@@ -1,4 +1,6 @@
 ﻿'use strict';
 app.controller("mapController", ["$scope", function ($scope) {
+
     $scope.googleMapsUrl = "https://maps.googleapis.com/maps/api/js?key=AIzaSyDCezs9lCKQtmKlP8mm_fBWZt25DlXoUjg&callback=initMap";
+
 }]);
