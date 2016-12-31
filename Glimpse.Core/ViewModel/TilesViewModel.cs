17fdf5f3dda6b07eea43332811f7e463340c3fd6
Joseph Daaboul <joseph.jd.daaboul@gmail.com>
2016-12-31 15:41:30 -0500

    Viewmodel responsible of getting promotions and using services #99
    makes a join to see promotions with location, uses geolocation service
    and google web service for duration

--- Glimpse.Core/ViewModel/TilesViewModel.cs
+++ Glimpse.Core/ViewModel/TilesViewModel.cs
@@ -1,9 +1,15 @@
 ﻿using Glimpse.Core.Contracts.Services;
+using Glimpse.Core.Model;
+using Glimpse.Core.Model.CustomModels;
 using Glimpse.Core.Services.General;
 using MvvmCross.Core.ViewModels;
 using MvvmCross.Plugins.Messenger;
+using Plugin.Geolocator;
+using Plugin.Geolocator.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Glimpse.Core.ViewModel
@@ -13,27 +19,108 @@ namespace Glimpse.Core.ViewModel
         private string _currentLanguage;
         private List<string> _languages;
         private IPromotionDataService _promotionDataService;
+        private IVendorDataService _vendorDataService;
+        private List<PromotionWithLocation> _promotions;
 
-        public TilesViewModel(IMvxMessenger messenger, IPromotionDataService promotionDataService) : base(messenger)
+        private IGeolocator locator;
+        private Location _userLocation;
+
+        private GoogleWebService _gwb;
+
+
+        public TilesViewModel(IMvxMessenger messenger, IPromotionDataService promotionDataService, IVendorDataService vendorDataService) : base(messenger)
         {
             _promotionDataService = promotionDataService;
+            _vendorDataService = vendorDataService;
+        }
+
+        public override async void Start()
+        {
+            base.Start();
+            await ReloadDataAsync();
+        }
+
+        protected override async Task InitializeAsync()
+        {
+            //Creates the locator
+            locator = CrossGeolocator.Current;
+            locator.DesiredAccuracy = 5;
+
+            //creates the google web service wrapper
+            _gwb = new GoogleWebService();
+
+            //get initial user location
+            _userLocation = await GetUserLocation();
+
+            PromotionList = await getPromotionsWithLocation();
         }
 
 
-
-        public string CurrentLanguage
+        public async Task<Location> GetUserLocation()
         {
-            get
+            //Get the current location            
+            var position = await locator.GetPositionAsync(timeoutMilliseconds: 10000);
+
+
+            return new Location()
             {
-                return _currentLanguage;
+                Lat = position.Latitude,
+                Lng = position.Longitude
+            };
+        }
+
+
+        private async Task<List<PromotionWithLocation>> getPromotionsWithLocation()
+        {
+            List<Promotion> allPromotions = await _promotionDataService.GetPromotions();
+            List<Vendor> allVendors = await _vendorDataService.GetVendors();
+
+            DateTime now = DateTime.Now;            
+
+            List<Promotion> activePromotions = allPromotions.Where(e => e.PromotionStartDate.CompareTo(now) <= 0  && e.PromotionEndDate.CompareTo(now) >= 0).ToList();
+
+            var mapPromotions = allVendors.Join(activePromotions, e => e.VendorId, b => b.VendorId,
+                                (e, b) => new PromotionWithLocation
+                                {
+                                    Title = b.Title,
+                                    Location = e.Location,
+                                    Description = b.Description,
+                                    CompanyName = e.CompanyName,
+                                    Duration = 9999,
+                                    Image = b.PromotionImage                                             
+                                }).ToList();
+
+            List<Location> promotionLocations = mapPromotions.Select(promotionWithLocation => promotionWithLocation.Location).ToList();
+
+            DistanceMatrix distanceMatrix = await _gwb.GetMultipleDurationResponse(_userLocation, promotionLocations);
+
+            
+            for(int i = 0; i < mapPromotions.Count; i++)
+            {
+                if(distanceMatrix.rows[0].elements[i].status.Equals("OK"))
+                {
+                    mapPromotions[i].Duration = distanceMatrix.rows[0].elements[i].duration.value;
+                }
+               
             }
+
+            List<PromotionWithLocation> final = mapPromotions.OrderBy(promotion => promotion.Duration).ToList().FindAll(p => p.Duration != 9999);
+            //GetMultipleDurationResponse(Location origin, List < Location > destination)
+
+            return final;
+        }
+
+        public List<PromotionWithLocation> PromotionList
+        {
+            get { return _promotions; }
             set
             {
-                _currentLanguage = value;
-                RaisePropertyChanged(() => CurrentLanguage);
+                _promotions = value;
+                RaisePropertyChanged(() => PromotionList);
             }
-        }
 
+        }
+      
 
 
     }
