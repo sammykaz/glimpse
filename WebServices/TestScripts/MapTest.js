545fb3e5902b6b6ef6aac22dbfee3346b16c43ed
asmalaaribi <asmalaaribi@gmail.com>
2016-12-31 12:31:08 -0500

    #87 unit test for google maps

---
+++ WebServices/TestScripts/MapTest.js
@@ -0,0 +1,16 @@
+﻿
+describe('modalController', function () {
+    var $controller, $rootScope;
+    beforeEach(angular.mock.module('myApp'));
+
+    beforeEach(angular.mock.inject(function (_$controller_, _$rootScope_) {
+        $controller = _$controller_;
+        $rootScope = _$rootScope_;
+    }));
+
+    it('should have the google maps url initialized', function () {
+        var scope = $rootScope.$new();
+        var controller = $controller('mapController', { $scope: scope });
+        expect(scope.googleMapsUrl).toBe("https://maps.googleapis.com/maps/api/js?key=AIzaSyDCezs9lCKQtmKlP8mm_fBWZt25DlXoUjg&callback=initMap");
+    });
+});
