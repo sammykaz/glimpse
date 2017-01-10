ac9dd89b3bae464e5347c4eb858732d68770b9e9
Joseph Daaboul <joseph.jd.daaboul@gmail.com>
2017-01-09 20:53:06 -0500

    #8 model for promotion image

---
+++ Glimpse.Core/Model/PromotionImage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Glimpse.Core.Model
+{
+    public class PromotionImage
+    {              
+        public int PromotionImageId { get; set; }
+
+        public byte[] Image { get; set; }    
+    
+    }
+}
