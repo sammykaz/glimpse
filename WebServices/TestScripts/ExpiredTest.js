ff6946a07747ea7eccc771a73a21a6d3b4bdf43f
asmalaaribi <asmalaaribi@gmail.com>
2017-01-11 12:47:41 -0500

    #118 unit test to test expired label

---
+++ WebServices/TestScripts/ExpiredTest.js
@@ -0,0 +1,44 @@
+﻿describe('PromotionController', function () {
+    var $controller, $rootScope;
+    beforeEach(angular.mock.module('myApp'));
+
+    beforeEach(angular.mock.inject(function (_$controller_, _$rootScope_) {
+        $controller = _$controller_;
+        $rootScope = _$rootScope_;
+    }));
+
+    it('should test expired date', function () {
+        var scope = $rootScope.$new();
+        var controller = $controller('PromotionController', {
+            $scope: scope,
+            $state: {},
+            $uibModal: {}
+        });
+
+
+        expect(scope.isPromotionExpired(new Date("01-02-2015"))).toBe(true);
+        expect(scope.isPromotionExpired(new Date("21-02-2018"))).toBe(false);
+    });
+
+
+    it('should have defined editPromotion method', function () {
+        var scope = $rootScope.$new();
+        var controller = $controller('PromotionController', {
+            $scope: scope,
+            $state: {},
+            $uibModal: {}
+        });
+        expect(scope.editPromotion).toBeDefined();
+    });
+
+    it('should have defined deletePromotion method', function () {
+        var scope = $rootScope.$new();
+        var controller = $controller('PromotionController', {
+            $scope: scope,
+            $state: {},
+            $uibModal: {}
+        });
+        expect(scope.deletePromotion).toBeDefined();
+    });
+
+});
