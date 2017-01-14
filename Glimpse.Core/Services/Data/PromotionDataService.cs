8dc76a57bb66d50020c744c19cafda43657ae342
Sam Kazerouni <samkazerouni@gmail.com>
2017-01-14 00:12:28 -0500

    Removed commented code

--- Glimpse.Core/Services/Data/PromotionDataService.cs
+++ Glimpse.Core/Services/Data/PromotionDataService.cs
@@ -42,22 +42,12 @@ namespace Glimpse.Core.Services.Data
             List<Promotion> allPromotions = await promotionRepository.GetPromotions();
             List<Vendor> allVendors = await vendorRepository.GetVendors();
 
+            //Get unique vendors
+            var uniqueVendors = allVendors.GroupBy(x => new { x.Location.Lat, x.Location.Lng }).Select(g => g.First()).ToList();
+
             List<Promotion> activePromotions = allPromotions.Where(e => e.PromotionStartDate.CompareTo(DateTime.Now) <= 0 && e.PromotionEndDate.CompareTo(DateTime.Now) >= 0).ToList();
 
-            /*
-            var mapPromotions = allVendors.Join(activePromotions, e => e.VendorId, b => b.VendorId,
-                                (e, b) => new
-                                {
-                                    e.CompanyName,
-                                    e.Location,
-                                    b.Title,
-                                    b.Description,
-                                    b.PromotionImage,
-                                    b.PromotionEndDate,
-                                    b.PromotionId
-                                });
-            */
-            var mapPromotions = allVendors.Join(activePromotions, e => e.VendorId, b => b.VendorId,
+            var mapPromotions = uniqueVendors.Join(activePromotions, e => e.VendorId, b => b.VendorId,
                 (e, b) => new PromotionWithLocation
                 {
                     Title = b.Title,
