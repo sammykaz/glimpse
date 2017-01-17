02c9c657679f01beddfe2eec12862d2849149d56
asmalaaribi <asmalaaribi@gmail.com>
2017-01-16 21:52:55 -0500

    updated unit tests

--- WebServices/TestScripts/PromoTest.js
+++ WebServices/TestScripts/PromoTest.js
@@ -10,33 +10,34 @@ describe('modalController', function () {
 
     it('should have the reset filter set as false initially', function () {
         var scope = $rootScope.$new();
-        var controller = $controller('modalController', { $scope: scope, $uibModalInstance: {}, promotionDetails: {} });
+        var controller = $controller('modalController', { $scope: scope, $uibModalInstance: {}, promotionDetails: {}, edit: {} });
         expect(scope.isResetEnable).toBe(false);
     });
 
     it('should set crop enable as false initially', function () {
         var scope = $rootScope.$new();
-        var controller = $controller('modalController', { $scope: scope, $uibModalInstance: {}, promotionDetails: {} });
+        var controller = $controller('modalController', { $scope: scope, $uibModalInstance: {}, promotionDetails: {}, edit: {} });
         expect(scope.isCropImageEnable).toBe(false);
     });
 
     it('should have saveCrop set as false initially', function () {
         var scope = $rootScope.$new();
-        var controller = $controller('modalController', { $scope: scope, $uibModalInstance: {}, promotionDetails: {} });
+        var controller = $controller('modalController', { $scope: scope, $uibModalInstance: {}, promotionDetails: {}, edit: {} });
         expect(scope.saveCrop).toBe(false);
     });
 
-    it('should have isCropImageEnable and saveCrop enabled when the cropImage method is called', function () {
+    it('should be able to call cropImage method', function () {
         var scope = $rootScope.$new();
-        var controller = $controller('modalController', { $scope: scope, $uibModalInstance: {}, promotionDetails: {} });
+        var controller = $controller('modalController', { $scope: scope, $uibModalInstance: {}, promotionDetails: {}, edit: {} });
+        var spy = spyOn(scope, 'cropImage');
         scope.cropImage();
-        expect(scope.saveCrop).toBe(true);
-        expect(scope.isCropImageEnable).toBe(true);
+        expect(scope.cropImage).toBeDefined();
+        expect(spy).toHaveBeenCalled();
     });
 
     it('should have isCropImageEnable and saveCrop disabled when the cancelCrop method is called', function () {
         var scope = $rootScope.$new();
-        var controller = $controller('modalController', { $scope: scope, $uibModalInstance: {}, promotionDetails: {} });
+        var controller = $controller('modalController', { $scope: scope, $uibModalInstance: {}, promotionDetails: {}, edit: {} });
         scope.cancelCrop();
         expect(scope.saveCrop).toBe(false);
         expect(scope.isCropImageEnable).toBe(false);
@@ -44,12 +45,12 @@ describe('modalController', function () {
 
     it('should have clear image and hide crop preview when the removeImage method is called', function () {
         var scope = $rootScope.$new();
-        var controller = $controller('modalController', { $scope: scope, $uibModalInstance: {}, promotionDetails: {} });
+        var controller = $controller('modalController', { $scope: scope, $uibModalInstance: {}, promotionDetails: {}, edit: {} });
         scope.removeImage();
         expect(scope.picFile).toBe(null);
         expect(scope.croppedDataUrl).toBe(null);
         expect(scope.picFile).toBe(null);
-        expect(scope.previewImage).toBe(null);
+        expect(scope.previewImage).toBe('');
         expect(scope.isCropImageEnable).toBe(false);
     });
     /*it('should have apply filter initially set to selectedFilter and when calling applyfilter its value should change to the selected parameter', function () {
