d23f85768713a1fd2c261023194faa680b797c7c
Joseph Daaboul <joseph.jd.daaboul@gmail.com>
2017-01-27 15:28:56 -0500

    #175 services for SQLlite

---
+++ Glimpse.Core/Contracts/Services/ILocalPromotionDataService.cs
@@ -0,0 +1,18 @@
+﻿using Glimpse.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Glimpse.Core.Contracts.Services
+{
+    public interface ILocalPromotionDataService
+    {
+        void Insert(PromotionWithLocation promotionWithLocation);
+
+        void Delete(PromotionWithLocation promotionWithLocation);
+
+        int Count { get; }
+    }
+}
---
+++ Glimpse.Core/Services/Data/Local/LocalPromotionDataService.cs
@@ -0,0 +1,45 @@
+﻿using Glimpse.Core.Contracts.Services;
+using SQLite.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Glimpse.Core.Model;
+using MvvmCross.Platform;
+using SQLite.Net.Async;
+
+namespace Glimpse.Core.Services.Data.Local
+{
+    public class LocalPromotionDataService : ILocalPromotionDataService
+    {
+        
+
+
+
+
+        public LocalPromotionDataService()
+        {
+
+        }
+
+
+        public int Count
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void Delete(PromotionWithLocation promotionWithLocation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(PromotionWithLocation promotionWithLocation)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
