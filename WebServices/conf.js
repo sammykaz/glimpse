970eabc6dd7488e1b59317e9a48e86dfb71eaa8e
Omer <bosniak.bos@gmail.com>
2017-02-01 18:29:47 -0500

    - Setting up Protractor end to end testing

---
+++ WebServices/conf.js
@@ -0,0 +1,18 @@
+﻿// An example configuration file.
+exports.config = {
+  directConnect: true,
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  // Capabilities to be passed to the webdriver instance.
+  capabilities: {
+    'browserName': 'chrome'
+  },
+
+  // Framework to use. Jasmine is recommended.
+  framework: 'jasmine',
+
+  // Spec patterns are relative to the current working directory when
+  // protractor is called.
+  // Options to be passed to Jasmine.
+  jasmineNodeOpts: {
+  }
+};
