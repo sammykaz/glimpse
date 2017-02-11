e39b25744332ae52350dda5bbd7a0f4feb64ab97
Sam Kazerouni <samkazerouni@gmail.com>
2017-02-11 02:11:48 -0500

    Revert "added powershell scripts for continuous integration"
    This reverts commit 0f853ec74c628dceab4f1405369b275319c64b5a.

--- build.ps1
+++
@@ -1,18 +0,0 @@
-﻿cls
-
-# '[p]sake' is the same as 'psake' but $Error is not pollutedremove-module [p]sake
-
-# find psake's path$psakeModule = (Get-ChildItem (".\Packages\psake*\tools\psake.psm1")).FullName | Sort-Object $_ | select -last 1 Import-Module $psakeModule
-
-Invoke-psake -buildFile .\Build\default.ps1 `
-			 -taskList Test `
-			 -framework 4.5.2 `
-			 -properties @{
-					"buildConfiguration" = "Debug"
-					"buildPlatform" = "Any CPU"} `
-			 -parameters @{"solutionFile"="..\glimpse.sln"}
-
-Write-Host "Build exit code:" $LastExitCode
-
-# Propagating the exit code so that builds actually fail when there is a problem
-exit $LastExitCode
