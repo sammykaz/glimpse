708fb0642e5e90fccfcd101e9e74a9e441244d18
Omer <bosniak.bos@gmail.com>
2017-02-12 13:00:41 -0500

    - Fixing url for map images - Fixing the image dimension

--- WebServices/src/controllers/mapController.js
+++ WebServices/src/controllers/mapController.js
@@ -74,7 +74,7 @@ app.controller('ShowPromotionController', ['$scope', '$uibModalInstance', 'promo
     $scope.Description = promotionDetails.Description;
     $scope.PromotionStartDate = promotionDetails.PromotionStartDate;
     $scope.PromotionEndDate = promotionDetails.PromotionEndDate;
-    $scope.previewImage = promotionDetails.PromotionImageURL ? "https://storageglimpse.blob.core.windows.net/imagestorage/" + promotionDetails.PromotionImageURL : '';
+    $scope.previewImage = promotionDetails.PromotionImageURL ? "https://glimpseimages.blob.core.windows.net/imagestorage/" + promotionDetails.PromotionImageURL : '';
 
     $scope.ok = function () {
         $uibModalInstance.close();
