4dc69ad47f824d726015310f8e86e30088847a70
asmalaaribi <asmalaaribi@gmail.com>
2017-02-17 10:39:31 -0500

    #212 added slider functionality in the promotioncontroller

--- WebServices/src/controllers/promotionController.js
+++ WebServices/src/controllers/promotionController.js
@@ -63,6 +63,27 @@ app.controller('modalImageController', function ($scope, $timeout, dataService, 
             $scope.images.push($scope.promotionImages[i].ImageURL);
         }
     }
+    $scope.direction = 'left';
+    $scope.currentIndex = 0;
+
+    $scope.setCurrentSlideIndex = function (index) {
+        $scope.direction = (index > $scope.currentIndex) ? 'left' : 'right';
+        $scope.currentIndex = index;
+    };
+
+    $scope.isCurrentSlideIndex = function (index) {
+        return $scope.currentIndex === index;
+    };
+
+    $scope.prevSlide = function () {
+        $scope.direction = 'left';
+        $scope.currentIndex = ($scope.currentIndex < $scope.images.length - 1) ? ++$scope.currentIndex : 0;
+    };
+
+    $scope.nextSlide = function () {
+        $scope.direction = 'right';
+        $scope.currentIndex = ($scope.currentIndex > 0) ? --$scope.currentIndex : $scope.images.length - 1;
+    };
     console.log(promotionDetails);
 
 });
