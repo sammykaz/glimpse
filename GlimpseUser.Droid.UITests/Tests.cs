f8e140903416eb6be52c8f5fe2014054e2965abf
RiceAndBeanz <eleonrodas@gmail.com>
2017-02-26 20:03:08 -0500

    added ui test project

---
+++ GlimpseUser.Droid.UITests/Tests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+using Xamarin.UITest.Android;
+
+namespace GlimpseUser.Droid.UITests
+{
+    [TestFixture]
+    public class Tests
+    {
+        AndroidApp app;
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            // TODO: If the Android app being tested is included in the solution then open
+            // the Unit Tests window, right click Test Apps, select Add App Project
+            // and select the app projects that should be tested.
+            app = ConfigureApp
+                .Android
+                .ApkFile ("../../../Android/bin/Debug/Glimpse.Droid.Glimpse.Droid-x86-Signed.apk")
+                .StartApp();
+        }
+
+        [Test]
+        public void AppLaunches()
+        {
+            app.Screenshot("First screen.");
+        }
+    }
+}
+
