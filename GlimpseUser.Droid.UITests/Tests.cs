be802c262e278ac505977a0400565aa1768861ce
RiceAndBeanz <eleonrodas@gmail.com>
2017-03-03 21:17:30 -0500

    #234

--- GlimpseUser.Droid.UITests/Tests.cs
+++ GlimpseUser.Droid.UITests/Tests.cs
@@ -21,7 +21,7 @@ namespace GlimpseUser.Droid.UITests
             // and select the app projects that should be tested.
             app = ConfigureApp
                 .Android
-                .ApkFile ("../../../Android/bin/Debug/Glimpse.Droid.Glimpse.Droid-x86-Signed.apk")
+                .ApkFile ("../../../Glimpse.Droid/bin/Release/Glimpse.Droid.Glimpse.Droid-x86-Signed.apk")
                 .StartApp();
         }
 
@@ -30,6 +30,30 @@ namespace GlimpseUser.Droid.UITests
         {
             app.Screenshot("First screen.");
         }
+
+        [Test]
+        public void NewTest()
+        {
+            app.Tap(x => x.Id("btnSignIn"));
+            app.Tap(x => x.Id("txtEmail"));
+            app.EnterText(x => x.Id("txtEmail"), "e9@gmail.com");
+            app.Tap(x => x.Id("txtPassword"));
+            app.EnterText(x => x.Id("txtPassword"), "e9");
+            app.Tap(x => x.Id("btnSignIn"));
+            app.SwipeRightToLeft();
+            app.SwipeLeftToRight();
+            app.Tap(x => x.Text("Footwear"));
+            app.Tap(x => x.Text("Electronics"));
+            app.Tap(x => x.Class("AppCompatImageView").Index(1));
+            app.Tap(x => x.Class("AppCompatImageView").Index(2));
+            app.ScrollUp();
+            app.SwipeLeftToRight();
+            app.SwipeRightToLeft();
+            app.Tap(x => x.Marked("Google Map"));
+            app.SwipeLeftToRight();
+            app.SwipeLeftToRight();
+        }
+
     }
 }
 
