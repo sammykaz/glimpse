6eada362bcb1bdf0c7794978377fa807f38d24b7
RiceAndBeanz <eleonrodas@gmail.com>
2017-03-05 03:09:36 -0500

    #234  TestNavigateThroughAllTheAppPages()

--- GlimpseUser.Droid.UITests/Tests.cs
+++ GlimpseUser.Droid.UITests/Tests.cs
@@ -48,8 +48,32 @@ namespace GlimpseUser.Droid.UITests
             app.SwipeLeftToRight();
             app.Screenshot("Swiping Top Card");
             app.SwipeRightToLeft();
-            app.Screenshot("Third Top Card");
+            app.Screenshot("Second Top Card");
         }
+
+        [Test]
+        public void TestNavigateThroughAllTheAppPages()
+        {
+            app.Tap(x => x.Id("btnSignIn"));
+            app.Tap(x => x.Id("txtEmail"));
+            app.EnterText(x => x.Id("txtEmail"), "e5@gmail.com");
+            app.Tap(x => x.Id("txtPassword"));
+            app.EnterText(x => x.Id("txtPassword"), "e5");
+            app.Tap(x => x.Id("btnSignIn"));
+            app.Tap(x => x.Id("cardImage").Index(3));
+            app.Screenshot("Detail view");
+            app.Back();
+            app.Screenshot("Card View");
+            app.Tap(x => x.Class("AppCompatImageView").Index(1));
+            app.Screenshot("LikedView");
+            app.Tap(x => x.Class("AppCompatImageView").Index(2));
+            app.Screenshot("MapView");
+            app.Tap(x => x.Marked("Google Map"));
+            app.Back();
+        }
+
+        
+
 
 
         
