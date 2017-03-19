57c830caaef8f092d76b257388349d278d1946c5
Sam Kazerouni <samkazerouni@gmail.com>
2017-03-18 22:26:46 -0400

    Changed BitmapProcessing to be asynchronous

--- Glimpse.Droid/Helpers/BitmapProcessing.cs
+++ Glimpse.Droid/Helpers/BitmapProcessing.cs
@@ -11,48 +11,44 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using System.Threading.Tasks;
 
 namespace Glimpse.Droid.Helpers
 {
     public static class BitmapProcessing
     {
+
         public static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight)
         {
             // Raw height and width of image
-            int height = options.OutHeight;
-            int width = options.OutWidth;
-            int inSampleSize = 1;
+            float height = options.OutHeight;
+            float width = options.OutWidth;
+            double inSampleSize = 1D;
 
             if (height > reqHeight || width > reqWidth)
             {
-                 int halfHeight = height / 2;
-                 int halfWidth = width / 2;
+                int halfHeight = (int)(height / 2);
+                int halfWidth = (int)(width / 2);
 
-                // Calculate the largest inSampleSize value that is a power of 2 and keeps both
-                // height and width larger than the requested height and width.
-                while ((halfHeight / inSampleSize) >= reqHeight
-                        && (halfWidth / inSampleSize) >= reqWidth)
+                // Calculate a inSampleSize that is a power of 2 - the decoder will use a value that is a power of two anyway.
+                while ((halfHeight / inSampleSize) > reqHeight && (halfWidth / inSampleSize) > reqWidth)
                 {
                     inSampleSize *= 2;
                 }
             }
 
-            return inSampleSize;
+            return (int) inSampleSize;
         }
 
-        public static Bitmap decodeSampledBitmapFromResource(Resources res, int resId, int reqWidth, int reqHeight)
+        public static async Task<Bitmap> LoadScaledDownBitmapForDisplayAsync(Resources res, BitmapFactory.Options options, int reqWidth, int reqHeight, int resourceId)
         {
-            // First decode with inJustDecodeBounds=true to check dimensions
-            BitmapFactory.Options options = new BitmapFactory.Options();
-            options.InJustDecodeBounds = true;
-            BitmapFactory.DecodeResource(res, resId, options);
-
             // Calculate inSampleSize
             options.InSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
 
             // Decode bitmap with inSampleSize set
             options.InJustDecodeBounds = false;
-            return BitmapFactory.DecodeResource(res, resId, options);
+            
+            return await BitmapFactory.DecodeResourceAsync(res, resourceId,options);
         }
     }
 }
